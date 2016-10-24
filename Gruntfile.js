5f916189a0d30216c57053c5cd9f5ddbac48c940
Sebastian Rojek <seriu54@gmail.com>
2016-10-24 19:06:16 +0200

    "clean the mess in file system"

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'CSS/style.css': 'SASS/style.sass'
+        }
+      }
+    },
+	imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'img/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'img/build/'
+        }]
+    },
+	watch: {
+    scripts: {
+        files: ['SASS/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false
+        }
+    }
+}
+}
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+ 
+
+  // Default task.
+  grunt.registerTask('default', ['sass', 'imagemin']);
+
+};
